7f8310ea4c49244155338c3fb69e065c1a0f20e9
Naresh <ghostwriternr@gmail.com>
2018-06-05 18:13:30 +0530

    Extract smaller boxes from grid

--- src/core.cpp
+++ src/core.cpp
@@ -7,6 +7,19 @@
 using namespace cv;
 using namespace std;
 
+RNG rng(12345);
+
+struct contour_sorter // 'less' for contours
+{
+    bool operator ()( const vector<Point>& a, const vector<Point> & b )
+    {
+        Rect ra(boundingRect(a));
+        Rect rb(boundingRect(b));
+        // scale factor for y should be larger than img.width
+        return ( (ra.x + 2000*ra.y) < (rb.x + 2000*rb.y) );
+    }
+};
+
 int main()
 {
 	// Load source image
@@ -81,6 +94,14 @@ int main()
     std::vector<std::vector<cv::Point> > contours;
 	cv::findContours(mask, contours, hierarchy, RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
+	// for( size_t i = 0; i< contours.size(); i++ )
+    // {
+    //     Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+    //     drawContours( rsz, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    // }
+	// imshow("contours", rsz);
+	// waitKey();
+
     vector<vector<Point> > contours_poly(contours.size());
     vector<Rect> boundRect(contours.size());
     vector<Mat> rois;
@@ -113,22 +134,54 @@ int main()
 		rois.push_back(rsz(boundRect[i]).clone());
 
 		// drawContours(rsz, contours, i, Scalar(0, 0, 255), CV_FILLED, 8, vector<Vec4i>(), 0, Point());
-		rectangle(rsz, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 1, 8, 0);
+		// rectangle(rsz, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 1, 8, 0);
 		break;
     }
 
-    for(size_t i = 0; i < rois.size(); ++i)
-    {
-        imshow("roi", rois[i]);
-        waitKey();
+	Mat timetableImage = rois[0];
+	Mat ttGray, ttBw, ttCopy;
+	ttCopy = timetableImage.clone();
+	cvtColor(timetableImage, ttGray, CV_BGR2GRAY);
+	adaptiveThreshold(~ttGray, ttBw, 255, CV_ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, -2);
+	hierarchy.clear();
+	contours.clear();
+	cv::findContours(ttBw, contours, hierarchy, RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	contours_poly.clear();
+	contours_poly.resize(contours.size());
+	boundRect.clear();
+	boundRect.resize(contours.size());
+	rois.clear();
+
+	std::sort(contours.begin(), contours.end(), contour_sorter());
+
+	for (size_t i = 0; i < contours.size(); i++) {
+		if (hierarchy[i][2] == -1) {
+			double area = contourArea(contours[i]);
+			if (area < 250)
+				continue;
+			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+        	boundRect[i] = boundingRect(Mat(contours_poly[i]));
+			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        	// drawContours( ttCopy, contours, i, color, 2, 8, hierarchy, 0, Point() );
+		rectangle(ttCopy, boundRect[i].tl(), boundRect[i].br(), color, 1, 8, 0);
+		rois.push_back(timetableImage(boundRect[i]).clone());
+		}
 	}
 
-    tesseract::TessBaseAPI *tess = new tesseract::TessBaseAPI();
-    // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (tess->Init(NULL, "eng")) {
-        fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
-    }
+	tesseract::TessBaseAPI *tess = new tesseract::TessBaseAPI();
+	// Initialize tesseract-ocr with English, without specifying tessdata path
+	if (tess->Init(NULL, "eng")) {
+	     fprintf(stderr, "Could not initialize tesseract.\n");
+	     exit(1);
+	}
+	for (size_t i=0; i<rois.size(); ++i) {
+		imshow("yolo", rois[i]);
+		waitKey();
+		tess->SetImage((uchar*)rois[i].data, rois[i].size().width, rois[i].size().height, rois[i].channels(), rois[i].step1());
+		tess->Recognize(0);
+		const char* out = tess->GetUTF8Text();
+		cout << out << endl;
+	}
 	tess->End();
 	return 0;
 }
