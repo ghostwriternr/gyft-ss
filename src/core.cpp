a9e7ea48102a779af30ed28f9e09cfeb5543c3ec
Naresh <ghostwriternr@gmail.com>
2018-06-30 03:36:38 +0530

    Club cells by row

--- src/core.cpp
+++ src/core.cpp
@@ -8,21 +8,55 @@ using namespace cv;
 using namespace std;
 
 RNG rng(12345);
+int tt_rows = 6;
+int tt_cols = 10;
 
-struct contour_sorter // 'less' for contours
-{
+struct contour_sorter {
   bool operator()(const pair<Mat, vector<Point>> &a,
                   const pair<Mat, vector<Point>> &b) {
     Rect ra(boundingRect(a.second));
     Rect rb(boundingRect(b.second));
-    // scale factor for y should be larger than img.width
-    // return ((ra.x + 2000 * ra.y) < (rb.x + 2000 * rb.y));
-    // printf("%d %d %d %d\n", ra.x, ra.y, rb.x, rb.y);
-    if (ra.y == rb.y)
+    if (fabs(ra.y - rb.y) < 5)
       return ra.x < rb.x;
     return ra.y < rb.y;
   }
 };
+
+struct y_sorter {
+  bool operator()(const pair<Mat, vector<Point>> &a,
+                  const pair<Mat, vector<Point>> &b) {
+    Rect ra(boundingRect(a.second));
+    Rect rb(boundingRect(b.second));
+    return ra.y < rb.y;
+  }
+};
+
+struct point_sorter {
+  bool operator()(Point pt1, Point pt2) { return (pt1.y < pt2.y); }
+};
+
+vector<int> group_cells(vector<pair<Mat, vector<Point>>> roi_contours) {
+  vector<int> starts = {0};
+  vector<pair<Mat, vector<Point>>> inter = roi_contours;
+  vector<pair<int, int>> inter_points;
+  for (size_t i = 0; i < inter.size(); ++i) {
+    inter_points.push_back(make_pair(boundingRect(inter[i].second).y, i));
+  }
+  for (size_t i = inter_points.size() - 1; i > 0; --i) {
+    inter_points[i].first = inter_points[i].first - inter_points[i - 1].first;
+  }
+  sort(inter_points.begin(), inter_points.end());
+  for (size_t i = inter_points.size() - tt_rows + 1; i < inter_points.size();
+       ++i) {
+    starts.push_back(inter_points[i].second);
+  }
+  sort(starts.begin(), starts.end());
+  for (size_t i = 0; i < starts.size(); ++i) {
+    cout << starts[i] << " ";
+  }
+  cout << endl;
+  return starts;
+}
 
 int main(int argc, char **argv) {
   // Load source image
@@ -201,6 +235,11 @@ int main(int argc, char **argv) {
   waitKey();
 
   std::sort(roi_contours.begin(), roi_contours.end(), contour_sorter());
+  for (size_t i = 0; i < roi_contours.size(); ++i) {
+    boundRect[i] = boundingRect(Mat(roi_contours[i].second));
+  }
+  cout << boundRect.size() << endl;
+  auto res = group_cells(roi_contours);
 
   tesseract::TessBaseAPI *tess = new tesseract::TessBaseAPI();
   // Initialize tesseract-ocr with English, without specifying tessdata path
@@ -208,6 +247,11 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Could not initialize tesseract.\n");
     exit(1);
   }
+
+  int tt_left = numeric_limits<int>::max();
+  int tt_right = numeric_limits<int>::lowest();
+  int tt_top = numeric_limits<int>::max();
+  int tt_bottom = numeric_limits<int>::lowest();
   for (size_t i = 0; i < roi_contours.size(); ++i) {
     Mat temp_gray, temp_bw;
     cvtColor(roi_contours[i].first, temp_gray, CV_BGR2GRAY);
@@ -224,7 +268,16 @@ int main(int argc, char **argv) {
       bitwise_not(temp_bw, temp_bw);
     fastNlMeansDenoising(temp_bw, temp_bw);
     imshow("yolo", temp_bw);
-    // cout << roi_contours[i].second << endl;
+    Rect temp_bounding(boundingRect(roi_contours[i].second));
+    cout << temp_bounding.x << " " << temp_bounding.y << endl;
+    if (temp_bounding.x < tt_left)
+      tt_left = temp_bounding.x;
+    if (temp_bounding.x < tt_top)
+      tt_top = temp_bounding.x;
+    if (temp_bounding.y + temp_bounding.width > tt_right)
+      tt_right = temp_bounding.y + temp_bounding.width;
+    if (temp_bounding.y + temp_bounding.height > tt_bottom)
+      tt_bottom = temp_bounding.y + temp_bounding.height;
     waitKey();
     tess->SetImage((uchar *)temp_bw.data, temp_bw.size().width,
                    temp_bw.size().height, temp_bw.channels(), temp_bw.step1());
@@ -233,6 +286,7 @@ int main(int argc, char **argv) {
     if (strlen(out) > 3)
       cout << out << endl;
   }
+  printf("%d %d %d %d\n", tt_left, tt_right, tt_top, tt_bottom);
   tess->End();
   return 0;
 }
