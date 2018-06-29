5922a889a3cde8eb3da47b804ba179adcfe0f4ec
Naresh <ghostwriternr@gmail.com>
2018-06-30 04:11:04 +0530

    Timetable extracted in an ideal world

--- src/core.cpp
+++ src/core.cpp
@@ -22,19 +22,6 @@ struct contour_sorter {
   }
 };
 
-struct y_sorter {
-  bool operator()(const pair<Mat, vector<Point>> &a,
-                  const pair<Mat, vector<Point>> &b) {
-    Rect ra(boundingRect(a.second));
-    Rect rb(boundingRect(b.second));
-    return ra.y < rb.y;
-  }
-};
-
-struct point_sorter {
-  bool operator()(Point pt1, Point pt2) { return (pt1.y < pt2.y); }
-};
-
 vector<int> group_cells(vector<pair<Mat, vector<Point>>> roi_contours) {
   vector<int> starts = {0};
   vector<pair<Mat, vector<Point>>> inter = roi_contours;
@@ -50,11 +37,8 @@ vector<int> group_cells(vector<pair<Mat, vector<Point>>> roi_contours) {
        ++i) {
     starts.push_back(inter_points[i].second);
   }
+  starts.push_back(roi_contours.size());
   sort(starts.begin(), starts.end());
-  for (size_t i = 0; i < starts.size(); ++i) {
-    cout << starts[i] << " ";
-  }
-  cout << endl;
   return starts;
 }
 
@@ -148,15 +132,6 @@ int main(int argc, char **argv) {
   cv::findContours(mask, contours, hierarchy, RETR_CCOMP,
                    CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-  // for( size_t i = 0; i< contours.size(); i++ )
-  // {
-  //     Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255),
-  //     rng.uniform(0,255) ); drawContours( rsz, contours, i, color, 2, 8,
-  //     hierarchy, 0, Point() );
-  // }
-  // imshow("contours", rsz);
-  // waitKey();
-
   vector<vector<Point>> contours_poly(contours.size());
   vector<Rect> boundRect(contours.size());
   vector<Mat> rois;
@@ -170,8 +145,7 @@ int main(int argc, char **argv) {
 
     // filter individual lines of blobs that might exist and they do not
     // represent a table
-    if (area < 100) // value is randomly chosen, you will need to find that by
-                    // yourself with trial and error procedure
+    if (area < 100) // value is randomly chosen, found by trial and error
       continue;
 
     approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
@@ -188,10 +162,6 @@ int main(int argc, char **argv) {
       continue;
 
     rois.push_back(rsz(boundRect[i]).clone());
-
-    // drawContours(rsz, contours, i, Scalar(0, 0, 255), CV_FILLED, 8,
-    // vector<Vec4i>(), 0, Point()); rectangle(rsz, boundRect[i].tl(),
-    // boundRect[i].br(), Scalar(0, 255, 0), 1, 8, 0);
     break;
   }
 
@@ -216,30 +186,26 @@ int main(int argc, char **argv) {
   for (size_t i = 0; i < contours.size(); i++) {
     if (hierarchy[i][2] == -1) {
       double area = contourArea(contours[i]);
-      // cout << area << " " << hierarchy[i] << endl;
       if (area < 800 || area > 10000)
         continue;
       approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
       boundRect[i] = boundingRect(Mat(contours_poly[i]));
       Scalar color =
           Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-      // drawContours( ttCopy, contours, i, color, 2, 8, hierarchy, 0, Point()
-      // );
       rectangle(ttCopy, boundRect[i].tl(), boundRect[i].br(), color, 3, 8, 0);
       roi_contours.push_back(
           make_pair(timetableImage(boundRect[i]).clone(), contours_poly[i]));
     }
   }
 
-  imshow("huha", ttCopy);
-  waitKey();
+  // imshow("huha", ttCopy);
+  // waitKey();
 
   std::sort(roi_contours.begin(), roi_contours.end(), contour_sorter());
   for (size_t i = 0; i < roi_contours.size(); ++i) {
     boundRect[i] = boundingRect(Mat(roi_contours[i].second));
   }
-  cout << boundRect.size() << endl;
-  auto res = group_cells(roi_contours);
+  vector<int> starts = group_cells(roi_contours);
 
   tesseract::TessBaseAPI *tess = new tesseract::TessBaseAPI();
   // Initialize tesseract-ocr with English, without specifying tessdata path
@@ -248,45 +214,47 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  int tt_left = numeric_limits<int>::max();
-  int tt_right = numeric_limits<int>::lowest();
-  int tt_top = numeric_limits<int>::max();
-  int tt_bottom = numeric_limits<int>::lowest();
-  for (size_t i = 0; i < roi_contours.size(); ++i) {
-    Mat temp_gray, temp_bw;
-    cvtColor(roi_contours[i].first, temp_gray, CV_BGR2GRAY);
-    threshold(temp_gray, temp_bw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-    double avg_brightness = 0;
-    for (int r = 0; r < temp_bw.rows; ++r) {
-      for (int c = 0; c < temp_bw.cols; ++c) {
-        int pix_val = temp_bw.at<uchar>(r, c);
-        avg_brightness += pix_val;
+  vector<vector<string>> timetable;
+  for (size_t i = 0; i < starts.size() - 1; ++i) {
+    vector<string> timetable_row;
+    for (int j = starts[i]; j < starts[i + 1]; ++j) {
+      Mat temp_gray, temp_bw;
+      cvtColor(roi_contours[j].first, temp_gray, CV_BGR2GRAY);
+      threshold(temp_gray, temp_bw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+      double avg_brightness = 0;
+      for (int r = 0; r < temp_bw.rows; ++r) {
+        for (int c = 0; c < temp_bw.cols; ++c) {
+          int pix_val = temp_bw.at<uchar>(r, c);
+          avg_brightness += pix_val;
+        }
+      }
+      avg_brightness /= (temp_bw.rows * temp_bw.cols);
+      if (avg_brightness < 128)
+        bitwise_not(temp_bw, temp_bw);
+      fastNlMeansDenoising(temp_bw, temp_bw);
+      // imshow("yolo", temp_bw);
+      // waitKey();
+      tess->SetImage((uchar *)temp_bw.data, temp_bw.size().width,
+                     temp_bw.size().height, temp_bw.channels(),
+                     temp_bw.step1());
+      tess->Recognize(0);
+      const char *out = tess->GetUTF8Text();
+      if (strlen(out) > 3) {
+        timetable_row.push_back(out);
+        cout << out << endl;
+      } else {
+        timetable_row.push_back("");
       }
     }
-    avg_brightness /= (temp_bw.rows * temp_bw.cols);
-    if (avg_brightness < 128)
-      bitwise_not(temp_bw, temp_bw);
-    fastNlMeansDenoising(temp_bw, temp_bw);
-    imshow("yolo", temp_bw);
-    Rect temp_bounding(boundingRect(roi_contours[i].second));
-    cout << temp_bounding.x << " " << temp_bounding.y << endl;
-    if (temp_bounding.x < tt_left)
-      tt_left = temp_bounding.x;
-    if (temp_bounding.x < tt_top)
-      tt_top = temp_bounding.x;
-    if (temp_bounding.y + temp_bounding.width > tt_right)
-      tt_right = temp_bounding.y + temp_bounding.width;
-    if (temp_bounding.y + temp_bounding.height > tt_bottom)
-      tt_bottom = temp_bounding.y + temp_bounding.height;
-    waitKey();
-    tess->SetImage((uchar *)temp_bw.data, temp_bw.size().width,
-                   temp_bw.size().height, temp_bw.channels(), temp_bw.step1());
-    tess->Recognize(0);
-    const char *out = tess->GetUTF8Text();
-    if (strlen(out) > 3)
-      cout << out << endl;
+    timetable.push_back(timetable_row);
   }
-  printf("%d %d %d %d\n", tt_left, tt_right, tt_top, tt_bottom);
   tess->End();
+  for (size_t i = 0; i < timetable.size(); ++i) {
+    for (size_t j = 0; j < timetable[i].size(); ++j) {
+      cout << timetable[i][j] << " || ";
+    }
+    cout << endl;
+    cout << "===============\n";
+  }
   return 0;
 }
